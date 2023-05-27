10f48648b7db893ff4700c15eb2937840fdefa53
Nica Tudor <zatalo.nox@gmail.com>
2023-05-27 15:29:08 +0300

    Choices

--- convertor.py
+++ convertor.py
@@ -1,3 +1,13 @@
 #Receving the user input
 
 user_input = float(input("Enter the temperature: "))
+
+#Choices
+
+print("--- What converter do you want? ---")
+print("1. Celsius to Fahrenheit")
+print("2. Celsius to Kelvin")
+print("3. Fahrenheit to Celsius")
+print("4. Fahrenheit to Kelvin")
+print("5. Kelvin to Celsius")
+print("6. Kelvin to Fahrenheit")
