2e1d8567833c70126a5ce57ba68cb416b646d12c
Nica Tudor <zatalo.nox@gmail.com>
2023-05-27 15:29:50 +0300

    Making decisions

--- convertor.py
+++ convertor.py
@@ -15,3 +15,18 @@ print("6. Kelvin to Fahrenheit")
 #User choice
 
 user_choice = int(input("Enter the corespondenting number: "))
+
+#Making decisions
+
+if user_choice == 1:
+    print((user_input * 9/5) + 32,  "° Fahrenheit")
+elif user_choice == 2:
+    print(user_input + 273.15, "° Kelvin")
+elif user_choice == 3:
+    print((user_input - 32) * 5/9, "° Celcius")
+elif user_choice == 4:
+    print((user_input - 32) * 5/9 + 273.15, "° Kelvin")
+elif user_choice == 5:
+    print(user_input - 273.15, "° Celcius")
+elif user_choice == 6:
+    print((user_input - 273.15) * 9/5 + 32, "° Fahrenheit")
